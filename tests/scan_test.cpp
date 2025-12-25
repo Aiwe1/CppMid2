0db05cff35bc77088f3deaf654cedbbb1c32e300
Aiwe1 <i-like-big-boobs@yandex.ru>
2025-12-25 16:48:24 +0000

    1

--- tests/scan_test.cpp
+++ tests/scan_test.cpp
@@ -1,9 +1,53 @@
+#include <cstdint>
 #include <gtest/gtest.h>
-#include <print>
 
 #include "scan.hpp"
 
 TEST(ScanTest, SimpleTest) {
     auto result = stdx::scan<std::string>("number", "{}");
-    ASSERT_FALSE(result);
+    ASSERT_FALSE(!result);
+}
+
+TEST(ScanTest, Int) {
+    auto result = stdx::scan<int>("3", "{%d}");
+    ASSERT_TRUE(std::get<0>(result.value().values()) == 3);
+}
+
+TEST(ScanTest, uint8) {
+    auto result = stdx::scan<int>("3", "{%u}");
+    ASSERT_TRUE(std::get<0>(result.value().values()) == 3);
+}
+
+TEST(ScanTest, uint8_error) {
+    auto result = stdx::scan<uint8_t>("-3", "{%u}");
+    ASSERT_TRUE(result.error().message == "Incorret value UINT");
+}
+
+TEST(ScanTest, string) {
+    auto result = stdx::scan<std::string>("alalala", "{%s}");
+    ASSERT_TRUE(std::get<0>(result.value().values()) == "alalala");
+}
+
+TEST(ScanTest, string_view) {
+    auto result = stdx::scan<std::string_view>("alalala", "{%s}");
+    ASSERT_TRUE(std::get<0>(result.value().values()) == "alalala");
+}
+
+TEST(ScanTest, Base) {
+    auto result = stdx::scan<int, float>("I want to sum 42 and 3.14 numbers.", "I want to sum{} and {%f} numbers.");
+    ASSERT_TRUE(std::get<0>(result.value().values()) == 42);
+    ASSERT_TRUE(std::get<1>(result.value().values()) == 3.14f);
+}
+
+TEST(ScanTest, double1) {
+    auto result =
+        stdx::scan<int, double>("I want to sum 42 and 3.14e+10 numbers.", "I want to sum{} and {%f} numbers.");
+    ASSERT_TRUE(std::get<0>(result.value().values()) == 42);
+    ASSERT_TRUE(std::get<1>(result.value().values()) == 31400000000.0);
+}
+
+TEST(ScanTest, not_double1) {
+    auto result = stdx::scan<int, float>("I want to sum 42 and 3.14e+10 numbers.", "I want to sum{} and {%f} numbers.");
+    ASSERT_TRUE(!result);
+    ASSERT_TRUE(result.error().message == "Incorrect format(float)");
 }
